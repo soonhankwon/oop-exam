c815227f176c9a818f48b6e4aad58d1cac9c338c
soonable@naver.com <soonable@naver.com>
2023-04-13 14:59:15 +0900

    feat : 캡슐화 (파라미터로 받은 객체의 메서드만 호출)

--- src/main/java/com/soon/oopexam/domain/Movie.java
+++ src/main/java/com/soon/oopexam/domain/Movie.java
@@ -5,12 +5,8 @@ public class Movie {
     public static final int NEW_RELEASE = 2;
     private int priceCode;
 
-    public boolean isPriceCodeNewRelease() {
-        return this.priceCode == NEW_RELEASE;
-    }
-
     public int getDaysRentedByPriceCode(int daysRented) {
-        if(isPriceCodeNewRelease() && daysRented > 1)
+        if(priceCode == NEW_RELEASE && daysRented > 1)
             return 2;
         else
             return 1;
